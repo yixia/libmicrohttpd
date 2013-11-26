979bbd37b4204d85f4aa7f254e61fe2266c5db1a
Crossle Song <sxm@yixia.com>
2013-11-26 18:08:18 +0800

    Build for Android

--- build_android.sh
+++ build_android.sh
@@ -13,9 +13,20 @@ SYSROOT=$ANDROID_NDK/platforms/android-18/arch-arm
 CROSS_PREFIX=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
 
 ./configure  --prefix=$PREFIX \
-  --host=$CROSS_PREFIX \
-  --enable-shared \
-  --with-sysroot=$SYSROOT
+  --host=arm-linux \
+  --enable-static \
+  --with-sysroot=$SYSROOT \
+  CC="${CROSS_PREFIX}gcc --sysroot=${SYSROOT}" \
+  CXX="${CROSS_PREFIX}g++ --sysroot=${SYSROOT}" \
+  RANLIB="${CROSS_PREFIX}ranlib" \
+  AR="${CROSS_PREFIX}ar" \
+  AR_FLAGS=rcu \
+  STRIP="${CROSS_PREFIX}strip" \
+  NM="${CROSS_PREFIX}nm" \
+  CFLAGS="-O3  --sysroot=${SYSROOT}" \
+  CXXFLAGS="-O3 --sysroot=${SYSROOT}" \
+  || exit 1
+
 
 make clean
 make -j4 install || exit 1
